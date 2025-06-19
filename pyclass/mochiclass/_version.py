68eacecf0f5c631ed7cd29da5be70871728aa2b0
Kushal Lodha <30127773+Kushallodha@users.noreply.github.com>
2025-06-20 04:22:52 +0900

    Delete pyclass/mochiclass directory
    Removing pyclass

--- pyclass/mochiclass/_version.py
+++
@@ -1,3 +0,0 @@
-class_version = '3.2.1b'
-url = 'https://github.com/mcataneo/mochi_class_public/archive/995126c.tar.gz'
-include = ('include', 'gravity_smg/include')  # includes relative to base class directory
